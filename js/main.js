70fc6d0f17f2ce0bada386eebf844a66a2bd9d27
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-06-16 14:42:31 +0300

    Коррективы в функции

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+ function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
     
-getRandomArbitrary()
-    /* генерирование целого числа из диапазона
-Источник: https://ask-dev.ru/info/1041/generating-random-whole-numbers-in-javascript-in-a-specific-range */
+getRandomInt()
+    /* генерирование случайного целого числа из диапазона
+Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5 */
 
 
 function random(min, max) {
     return min + Math.random() * (max - min);
       }
+      
+random ()
 /*Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. 
 Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
