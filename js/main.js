31980db104f83a8cfed7d5175e3f3287b0abc759
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-06-16 18:22:17 +0300

    Корректировка 2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
- function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-    
-getRandomInt()
-    /* генерирование случайного целого числа из диапазона
-Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5 */
+сonst getRandomInt = (min, max) => {
+    if (min < 0 || max < 0)
+    return -1;
+}
 
+if (min > max) {
+    [min, max] = [max, min];
 
-function random(min, max) {
-    return min + Math.random() * (max - min);
-      }
-      
-random ()
-/*Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. 
-Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
+    return Math.floor(<Math.random() * (max-min + 1) + min);
+}
+
+getRandomInt (1, 555)
+
+сonst getRandomFloat = (min, max, digit = 5) => {
+    if (min < 0 || max < 0)
+    return -1;
+}
+
+if (min > max) {
+    [min, max] = [max, min];
+
+    return (Math.random() * ((max-min) + min)).toFixed(digit);
+}
+
+getRandomFloat(1, 555)
+
+getRandomInt (1, 555)
