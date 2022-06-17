a428e8f29e2bc9810fb572ccd2c4efa5f8c496a6
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-06-17 15:29:00 +0300

    правки после консультации

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-const getRandomInt = (min, max) => {
-    if (min < 0 || max < 0)
-    return -1;
-}
+const getRandomNumber = (min, max) => {
+    if (min < 0 || max < 0){
+        return -1;}
 
-if (min > max) {
-    [min, max] = [max, min];
+    if (min > max) {
+        [min, max] = [max, min];
+    }
 
-    return Math.floor(<Math.random() * (max-min + 1) + min);
-}
+    return Math.floor(Math.random() * (max-min + 1) + min);
+};
 
-getRandomInt (1, 555)
 
-const getRandomFloat = (min, max, digit = 5) => {
-    if (min < 0 || max < 0)
-    return -1;
-}
-
-if (min > max) {
-    [min, max] = [max, min];
+const generateCoordinate = (min, max, digit = 5) => {
+    if (min < 0 || max < 0){
+        return -1;
+    }
+    
+    if (min > max) {
+        [min, max] = [max, min];
+    }
 
     return (Math.random() * ((max-min) + min)).toFixed(digit);
 }
-
-getRandomFloat(1, 555)
-
-getRandomInt (1, 555)
