6d7b7b2f8b952f10334da2aa23fc399ed7f3122d
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-06-24 16:28:55 +0300

    Правки после консультации

--- js/data.js
+++ js/data.js
@@ -2,9 +2,9 @@ const TYPES_APARTMENT = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES_APARTMENT = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS_APARTMENT = [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
 const MIN_PRICE = 200;
@@ -21,17 +21,18 @@ const offerDescription = 'Здесь могла быть Ваша реклама
 
 const RENTAL_AD_COUNT = 10;
 
-const DIGIT = 5; 
+const DIGIT = 5;
 
 const Lat = {
-    MIN: 35.65000,
-    MAX: 35.70000
-}
+  MIN: 35.65000,
+  MAX: 35.70000
+};
 
 const Lng = {
-    MIN: 139.70000,
-    MAX:139.80000
-}
+  MIN: 139.70000,
+  MAX: 139.80000
+};
 
 const COUNT = 10;
-const offers = [];
+
+export {TYPES_APARTMENT, TIMES, FEATURES_APARTMENT, PHOTOS_APARTMENT, MIN_PRICE, MAX_PRICE, MIN_ROOMS, MAX_ROOMS, MIN_GUESTS, MAX_GUESTS, offerTitle, offerDescription, RENTAL_AD_COUNT, DIGIT, Lat, Lng, COUNT};
