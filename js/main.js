0b23c0688f925a3b8a7f668b9fa71b94cdfef77e
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-06-30 16:08:05 +0300

    Первые правки

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 import { renderCard } from './data-generation.js';
+import { disableForm } from './formadj.js';
+import { enableForm } from './formadj.js';
+
 renderCard();
+disableForm();
+enableForm();
