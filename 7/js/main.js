1fda2f1e1807dc20d75035a9d3f949f047626104
Keks <keks@htmlacademy.ru>
2022-07-12 11:39:13 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+import { renderCard } from './data-generation.js';
+import { setDisabledState} from './formadj.js';
+import { toggleInteractive } from './formadj.js';
+
+renderCard();
+setDisabledState();
+toggleInteractive();
