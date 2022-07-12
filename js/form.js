e83e31c83ffc65ccfefad5773c668910a2437a95
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-07-12 11:26:23 +0300

    После 2-х консультаций

---
+++ js/form.js
@@ -0,0 +1,83 @@
+const PRICE_MAX = 100;
+
+const TITLE_LENGTH = {
+  min: 30,
+  max: 100,
+};
+
+const TYPE_OF_HOUSE = {
+  bungalow: '0',
+  flat: '1000',
+  house: '5000',
+  palace: '10000',
+};
+
+const adForm = document.querySelector('.ad-form');
+const type = adForm.querySelector('#type');
+const title = adForm.querySelector('#title');
+const price = adForm.querySelector('#price');
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+const guestNumber = capacity.querySelector('option');
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__element--error',
+});
+
+
+const validateTitle = (value) => value.length <= TITLE_LENGTH.max && value.length >= TITLE_LENGTH.min;
+const validatePrice = (value) => value <= PRICE_MAX && value >= TYPE_OF_HOUSE[type.value];
+const getErrorTextPrice = () => Number[price.value] < TYPE_OF_HOUSE[type.value] ? `Минимальная цена ${TYPE_OF_HOUSE[type.value]}`: `Максимальная цена ${PRICE_MAX}`;
+
+pristine.addValidator(title, validateTitle, 'От 30 до 100 символов');
+pristine.addValidator(price, validatePrice, getErrorTextPrice);
+
+
+const onTypeOfHouseChange = () => {
+  const minPrice = TYPE_OF_HOUSE[type.value];
+  price.placeholder = minPrice;
+  price.min = minPrice;
+};
+
+type.addEventListener('change', onTypeOfHouseChange);
+
+// количество комнат и количество мест
+const NumberOfGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+
+const validateRooms = () => {
+  const roomValue = roomNumber.value;
+
+  guestNumber.forEach((guest) => {
+    /*Цель найти  и деактивировать ненужные опции количеств комнат.
+    В объекте, в котором соотносятся количество гостей и комнат, по ключу, который равен количеству гостей, проходимся по массивам,
+    которые состоят из опций количества комнат.
+    В массивах, в которых мы не находим элемент со значением введенного количества гостей, мы возвращаем -1*/
+    const isDisabled = (NumberOfGuests[roomValue].indexOf(guest.value) === -1);
+    guest.selected = NumberOfGuests[roomValue][0] === guest.value;
+    guest.disabled = isDisabled;
+    guest.hidden = isDisabled;
+  });
+};
+
+validateRooms();
+
+const onRoomNumberChange = () => {
+  validateRooms();
+};
+
+roomNumber.addEventListener('change', onRoomNumberChange);
+
+
+adForm.addEventListener('submit', (evt) => {
+
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+});
