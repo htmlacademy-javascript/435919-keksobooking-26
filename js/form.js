d83cf9a628b1486a2a026b30da393447be6e36e0
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-07-15 09:38:21 +0300

    3

--- js/form.js
+++ js/form.js
@@ -19,10 +19,9 @@ const price = adForm.querySelector('#price');
 const roomNumber = adForm.querySelector('#room_number');
 const capacity = adForm.querySelector('#capacity');
 const guestNumber = capacity.querySelectorAll('option');
+const formTime = adForm.querySelector('.ad-form__element--time');
 const timeIn= adForm.querySelector('#timein');
-const timeInField = timeIn.querySelectorAll('option');
 const timeOut = adForm.querySelector('#timeout');
-const timeOutField = timeOut.querySelectorAll('option');
 
 const pristine = new Pristine(adForm, {
   classTo: 'ad-form__element',
@@ -79,9 +78,10 @@ const onRoomNumberChange = () => {
 
 roomNumber.addEventListener('change', onRoomNumberChange);
 
-
-timeIn.addEventListener('change', timeInField.value = timeOutField.value);
-timeOut.addEventListener('change', timeOutField.value = timeInField.value);
+formTime.addEventListener('change', (evt) => {
+  timeIn.value = evt.target.value;
+  timeOut.value = evt.target.value;
+});
 
 adForm.addEventListener('submit', (evt) => {
 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+import { offers } from './arrayOffers.js';
 import { renderCard } from './data-generation.js';
 import { setDisabledState} from './formadj.js';
 import './form.js';
 import { toggleInteractive } from './formadj.js';
-import { offers } from './arrayOffers.js';
+
 
 setDisabledState();
 toggleInteractive();
-renderCard();
 renderCard(offers[0]);
