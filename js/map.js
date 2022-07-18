ae67861f85d7ba69432dc74755521fd8913a6076
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-07-18 12:59:41 +0300

    17

--- js/map.js
+++ js/map.js
@@ -1,29 +1,23 @@
-// import { setDisabledState } from './formadj.js'; Эту функцию тоже нужно запускать?
-import { toggleInteractive } from './formadj.js';
-import { offers } from './arrayOffers.js';
-import { renderCard } from './data-generation.js';
-const adForm = document.querySelector('.ad-form');
-const addressField = adForm.querySelector('#address');
-const resetButton = adForm.querySelector('.ad-form__reset');
-
 const TOKIO_COORDINATES = {
   lat: 35.681729,
   lng: 139.753927,
 };
 const ZOOM_LEVEL = 10;
 
+
 const map = L.map('map-canvas')
-  .on('load', toggleInteractive)
+  .on('load', () => {
+  })
   .setView(TOKIO_COORDINATES, ZOOM_LEVEL);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }
 ).addTo(map);
 
-
+/*
 const mainPinIcon = L.icon({
   iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
@@ -40,7 +34,8 @@ const mainPinMarker = L.marker(
 mainPinMarker.addTo(map);
 
 mainPinMarker.on('moveend', (evt) => {
-  addressField.value = evt.target.getLatLng();
+  const { lat, lng } = evt.target.getLatLng();
+  addressField.value = `${lat.toFixed(FIXED_NUMBER)}, ${lng.toFixed(FIXED_NUMBER)}`;
 });
 
 const icon = L.icon({
@@ -51,9 +46,13 @@ const icon = L.icon({
 
 const createMarker = (offer) => {
   const marker = L.marker(
-    offer.address,
+    {
+      lat: offer.location.lat,
+      lng: offer.location.lng,
+    },
     {
       icon: icon,
+      keepInView: true,
     },
   );
   marker
@@ -63,8 +62,11 @@ const createMarker = (offer) => {
 
 offers.forEach(createMarker);
 
-resetButton.addEventListener('click', () => {
+const setDefaultState = () => {
   mainPinMarker.setLatLng(TOKIO_COORDINATES);
   map.setView(TOKIO_COORDINATES, ZOOM_LEVEL);
   map.closePopup();
-});
+};
+
+export {setDefaultState};
+*/
