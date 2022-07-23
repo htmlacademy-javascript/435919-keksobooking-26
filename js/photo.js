4573abc0eb9b1dd1b1b892d479abb4b2a627c9b5
ваше имяEvgeniya <evgeniya.aksenova.work@gmail.com>
2022-07-23 12:07:43 +0300

    11

--- js/photo.js
+++ js/photo.js
@@ -1,8 +1,8 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-const previewMuffin  = 'img/muffin-grey.svg';
 
 const headerPhoto = document.querySelector('.ad-form__field [type=file]');
 const previewHeaderPhoto = document.querySelector('.ad-form-header__preview__img');
+const previewDefault = previewHeaderPhoto.src;
 
 headerPhoto.addEventListener('change', () => {
   const file = headerPhoto.files[0];
@@ -18,6 +18,9 @@ headerPhoto.addEventListener('change', () => {
 const livingPhoto = document.querySelector('.ad-form__upload input[type=file]');
 const previewLivingPhotoForm = document.querySelector('.ad-form__photo');
 const previewLivingPhoto = document.createElement('img');
+previewLivingPhoto.width = '70';
+previewLivingPhoto.height = '70';
+//previewLivingPhoto.style.padding = '12px 12px';
 previewLivingPhotoForm.appendChild(previewLivingPhoto);
 
 
@@ -28,16 +31,13 @@ livingPhoto.addEventListener('change', () => {
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
-    file.on('load',previewLivingPhoto.src = URL.createObjectURL(file));
-    previewLivingPhoto.width = '70';
-    previewLivingPhoto.height = '70';
-    previewLivingPhoto.style.padding = '12px 12px';
+    file.on('load', previewLivingPhoto.src = URL.createObjectURL(file));
   }
 });
 
 const photoRemove = () =>{
-  previewHeaderPhoto.src = previewMuffin;
-  previewLivingPhoto.src = previewMuffin;
+  previewHeaderPhoto.src = previewDefault;
+  previewLivingPhoto.src = '';
 };
 
 export {photoRemove};
